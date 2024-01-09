07b51ebc383fd916e698c7a8b9000b048da032c2
theshyy1 <ronaldoisbestt@gmail.com>
2024-01-09 10:14:44 +0700

    add

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  return (
+    <div>
+      DetailPage
+      <h1>dsd</h1>
+    </div>
+  );
 };
 
 export default DetailPage;
